0a2dfd26703f76702b5869665a5eb1452a19c55a
Grantjos12 <grantjoslyn@gmail.com>
2020-12-17 18:15:14 -0600

    appending elements and forumals working

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const loan = document.querySelector("#loan");
 const interest = document.querySelector("#interest");
 const calculate = document.querySelector("#calculate");
 const period = document.querySelector("#years");
+const monthlyPayment = document.querySelector("#monthlyPayment");
+const totalPayment = document.querySelector("#totalPayment");
+const accured = document.querySelector("#accured");
 
 calculate.addEventListener("click", loanMath);
 
@@ -10,16 +13,17 @@ function loanMath(e) {
   console.log("clicked");
   const principal = parseFloat(loan.value);
   const interestRate = parseFloat(interest.value) / 100 / 12;
-  const calcPeriod = parseFloat(period.value) * 12;
+  const paymentPeriods = parseFloat(period.value) * 12;
 
   //Calculate Monthly Payment
   // Equation: monthly = p* r (1+rate)^n /(1+r)^2 -1
   //https://www.sitepoint.com/javascript-interest-loan-calculator-algorithm/
-  const x = Math.pow(1 + interestRate, calcPayments);
+  const x = Math.pow(1 + interestRate, paymentPeriods);
   const monthly = (principal * interestRate * x) / (x - 1);
-
-  if (isFinite(mothly)) {
-  }
-  // Total Payment
-  // Total Accrued Interest
+  const totalNew = (monthly * paymentPeriods).toFixed(2);
+  const totalAccured = (monthly * paymentPeriods - principal).toFixed(2);
+  //Appending variable Results to DOM
+  monthlyPayment.innerHTML = `${monthly.toFixed(2)}`;
+  totalPayment.innerHTML = `${totalNew}`;
+  accured.innerHTML = `${totalAccured}`;
 }
